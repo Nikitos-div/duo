ad36b805d9ade99e48fa1bcd732dc4d3a90445fe
“Nikita <n.churakov@sbereducation.ru>
2022-04-20 16:54:57 +0300

    delete warn

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
+    mode: 'development',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js'
