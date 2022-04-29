eb8c266eec91f86132fd2dbb7578649fc007d192
Nikita Churakov <motiki839@gmail.com>
2022-04-30 00:47:26 +0300

    :rocket:

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './src/client/index.tsx', // точка входа, о которой говорилось ранее.
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                      loader: 'ts-loader'
+                    },
+                  ],
+              },
+        
+            {
+                test: /\.css$/, // сопоставляет только файлы .css (т.е. не .scss и др.)
+                use: ['style-loader', 'css-loader'], 
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js', // выходной бандл
+    },
+    devServer: {
+      // contentBase
+      static : {
+        directory : path.join(__dirname, "public/")
+      },
+      port: 3000,
+      // publicPath
+    //   devMiddleware:{
+    //      publicPath: "https://localhost:3000/dist/",
+    //   },
+      // hotOnly
+      hot: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+        {
+            title: 'бля',
+            template: './public/index.html',
+        }
+    )],
+
+    devtool: 'eval-source-map', // создает высококачественные карты кода
+}
