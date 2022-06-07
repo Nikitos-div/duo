bba9efeac444c583b8084413e0960ca9dcbe7859
Nikita Churakov <motiki839@gmail.com>
2022-06-07 19:48:39 +0300

    commit

--- src/engine/main/Engine.ts
+++ src/engine/main/Engine.ts
@@ -2,9 +2,31 @@ import Konva from 'konva'
 import { Layer, LayerConfig } from 'konva/lib/Layer';
 import Shapes from './Shapes/index'
 import { Stage } from 'konva/lib/Stage'
-
+import createSelectionRectangle from '../utils/createSelectionRectangle';
 import { v4  } from 'uuid';
 import Store from '../store';
+import { Transform } from 'konva/lib/Util';
+
+
+const initStageEvents = (stage: Stage): void => {
+   
+}
+
+
+
+
+const createTransformer = (): Transform => {
+    return new Konva.Transform()
+}
+
+
+
+
+
+
+
+
+
 
 
 interface EngineConfig {
@@ -20,8 +42,18 @@ class Engine {
     constructor(config: EngineConfig, layerConfig?:LayerConfig) {
         this.stage = new Konva.Stage(config)
         this.layer = new Konva.Layer(layerConfig)
+        const transformer = createTransformer()
+        this.layer.add(transformer)
         this.stage.add(this.layer)
         this.store = new Store()
+
+
+        const selectionRectangle = createSelectionRectangle()
+        
+        this.layer.add(selectionRectangle)
+        initStageEvents(this.stage)
+        
+        
     }
     createShape(shapeName: string, config: unknown) {
         const shape = Shapes[shapeName](config)
---
+++ src/engine/utils/createSelectionRectangle.ts
@@ -0,0 +1,8 @@
+import Konva from "konva"
+
+export default () => {
+    return new Konva.Rect({
+        fill: 'rgba(0,0,255,0.5)',
+        visible: false,
+    })
+}
